b13f66f1c52b3bc2cad0cb23e4ca2a1571dcf2c4
w-mj <w.mingjian@yahoo.com>
2019-07-16 11:18:33 +0800

    more stable

--- app/src/main/java/com/dingdonginc/zhangfang/services/auto/fetch/accounts/ParseWechatDetail.kt
+++ app/src/main/java/com/dingdonginc/zhangfang/services/auto/fetch/accounts/ParseWechatDetail.kt
@@ -1,6 +1,7 @@
 package com.dingdonginc.zhangfang.services.auto.fetch.accounts
 import android.util.Log
 import android.view.accessibility.AccessibilityNodeInfo
+import java.lang.Exception
 
 object ParseWechatDetail {
     fun parse(item: AccessibilityNodeInfo) {
@@ -8,17 +9,32 @@ object ParseWechatDetail {
             Log.e(item.className.toString(), "class is not a webview")
             return
         }
-        val detailRoot = item.getChild(0).getChild(0)
-        Log.i("商家名", detailRoot.getChild(1).text.toString())
-        Log.i("金额", detailRoot.getChild(2).text.toString())
-        val moreDetail = detailRoot.getChild(3).getChild(0)
-        for (i in 1 until moreDetail.childCount step 2) {
-            val tag = moreDetail.getChild(i).text.toString()
-            if (tag in listOf("当前状态", "商品", "商户全称", "支付时间", "支付方式", "交易单号")) {
-                Log.i(tag, moreDetail.getChild(i + 1).getChild(0).text.toString())
-            } else if (tag in listOf("商户单号")) {
-                Log.i(tag, moreDetail.getChild(i + 2).getChild(0).text.toString())
+        var detailRoot:AccessibilityNodeInfo? = null
+        try{
+            detailRoot = item.getChild(0).getChild(0)
+            Log.i("商家名", detailRoot.getChild(1).text.toString())
+            Log.i("金额", detailRoot.getChild(2).text.toString())
+            val moreDetail = detailRoot.getChild(3).getChild(0)
+            for (i in 1 until moreDetail.childCount step 2) {
+                val child = moreDetail.getChild(i)
+                val tag = child.text.toString()
+                if (tag in listOf("当前状态", "商品", "商户全称", "支付时间",
+                        "转账时间", "支付方式", "交易单号", "转账单号")) {
+                    Log.i(tag, moreDetail.getChild(i + 1).getChild(0).text.toString())
+                } else if (tag in listOf("商户单号")) {
+                    val child1Str = moreDetail.getChild(i + 1).getChild(0).text.toString()
+                    var merchant:String?
+                    merchant = if (child1Str == "可在支持的商户扫码退款") {
+                        moreDetail.getChild(i + 2).getChild(0).text.toString()
+                    } else {
+                        child1Str
+                    }
+                    Log.i(tag, merchant)
+                }
+//                child.recycle()
             }
+        } catch (e: Exception) {
+            Log.e("Error", e.toString())
         }
     }
 }
