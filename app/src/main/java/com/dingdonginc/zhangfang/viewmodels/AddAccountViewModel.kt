ec1cab7f27dfbd6892282f729650ef00bc0e9644
w-mj <w.mingjian@yahoo.com>
2019-07-19 10:30:32 +0800

    digital keyboard

--- app/src/main/java/com/dingdonginc/zhangfang/viewmodels/AddAccountViewModel.kt
+++ app/src/main/java/com/dingdonginc/zhangfang/viewmodels/AddAccountViewModel.kt
@@ -1,7 +1,67 @@
 package com.dingdonginc.zhangfang.viewmodels
 
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.databinding.ObservableField
+import androidx.databinding.ObservableInt
 import androidx.lifecycle.ViewModel;
 
 class AddAccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val currentInput = ObservableField<String>()
+    val sum = 0
+
+    init {
+//        currentInput.set(0)
+        currentInput.set("")
+    }
+
+    private var kbState = 0
+
+
+    fun onDigitalKeyClick(view: View) {
+        val b: Button = view as Button
+        val str = currentInput.get()?: ""
+        if (str == "") {
+            if (b.text == "+" || b.text == "-")
+                return
+            if (b.text == ".")
+                currentInput.set("0.")
+            else
+                currentInput.set(b.text.toString())
+        } else {
+            if (b.text == "+" || b.text == "-") {
+                if (str.last() == '+' || str.last() == '-')
+                    return
+                if (str.last() == '.')
+                    currentInput.set(str.substring(0, str.lastIndex) + b.text)
+                else
+                    currentInput.set(str + b.text)
+            } else if (b.text == ".") {
+                var i = str.lastIndex
+                while (i >= 0) {
+                    if (str[i] == '+' || str[i] == '-')
+                        break
+                    if (str[i] == '.')
+                        return
+                    i--
+                }
+                if (i == str.lastIndex)
+                    currentInput.set("${str}0.")
+                else
+                    currentInput.set("$str.")
+            } else {
+                if (str.length > 3 && str[str.length - 3] == '.')
+                    return
+                currentInput.set(str + b.text.toString())
+            }
+        }
+    }
+
+    fun onBackspaceClick(view: View) {
+        val str = currentInput.get()?:""
+        if (str == "")
+            return
+        currentInput.set(str.substring(0, str.length - 1))
+    }
 }
--- app/src/main/java/com/dingdonginc/zhangfang/views/AddAccountFragment.kt
+++ app/src/main/java/com/dingdonginc/zhangfang/views/AddAccountFragment.kt
@@ -10,7 +10,6 @@ import androidx.databinding.DataBindingUtil
 
 import com.dingdonginc.zhangfang.R
 import com.dingdonginc.zhangfang.viewmodels.AddAccountViewModel
-import com.dingdonginc.zhangfang.viewmodels.DigitalKeyboardViewModel
 
 class AddAccountFragment : Fragment() {
 
@@ -19,7 +18,6 @@ class AddAccountFragment : Fragment() {
     }
 
     private lateinit var viewModel: AddAccountViewModel
-    private lateinit var digital_vm: DigitalKeyboardViewModel
     private lateinit var binding: com.dingdonginc.zhangfang.databinding.AddAccountFragmentBinding
 
     override fun onCreateView(
@@ -27,9 +25,8 @@ class AddAccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.add_account_fragment, container, false)
-        digital_vm = ViewModelProviders.of(this).get(DigitalKeyboardViewModel::class.java)
-        binding.digitalVm = digital_vm
-//        return inflater.inflate(R.layout.add_account_fragment, container, false)
+        viewModel = ViewModelProviders.of(this).get(AddAccountViewModel::class.java)
+        binding.vm = viewModel
         return binding.root
     }
 
