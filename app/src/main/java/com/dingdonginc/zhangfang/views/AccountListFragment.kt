65ae6cea290c7795d7a1c38593bf34ca7206ff7f
ZJH <215301160@qq.com>
2019-07-22 11:48:10 +0800

    home page select

--- app/src/main/java/com/dingdonginc/zhangfang/views/AccountListFragment.kt
+++ app/src/main/java/com/dingdonginc/zhangfang/views/AccountListFragment.kt
@@ -2,9 +2,13 @@ package com.dingdonginc.zhangfang.views
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -30,10 +34,35 @@ class AccountListFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         val fab = view!!.findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
-            val itent = Intent();
-            itent.setClass(context!!, AddAccountActivity::class.java);
+            val itent = Intent()
+            itent.setClass(context!!, AddAccountActivity::class.java)
             startActivity(itent);
             activity!!.finish()
         }
+        var list = ArrayList<String>()
+        list.add("白羊座")
+        list.add("金牛座")
+        list.add("双子座")
+        list.add("巨蟹座")
+        list.add("狮子座")
+        list.add("处女座")
+        list.add("天秤座")
+        list.add("天蝎座")
+        list.add("射手座")
+        list.add("摩羯座")
+        list.add("水瓶座")
+        list.add("双鱼座")
+
+        var adapter = ArrayAdapter<String>(this.context!!,R.layout.spinner_item_selected,list)
+        adapter.setDropDownViewResource(R.layout.spinner_item_drop)
+        var spiner = view!!.findViewById<Spinner>(R.id.spin)
+        Log.d(R.id.spin.toString(), R.id.spin.toString())
+        spiner.setAdapter(adapter)
+        spiner = view!!.findViewById(R.id.spin2)
+        spiner.setAdapter(adapter)
+        spiner = view!!.findViewById(R.id.spin3)
+        spiner.setAdapter(adapter)
+        spiner = view!!.findViewById(R.id.spin4)
+        spiner.setAdapter(adapter)
     }
 }
